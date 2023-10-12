bfe22401eb13016ff37557fa4cf17934c15145a0
kapil-malviya <122611483+kapil-malviya@users.noreply.github.com>
2023-10-12 18:36:51 +0530

    Update tests.py

--- serializer1/testapp/tests.py
+++ serializer1/testapp/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+'''
+This is a separate application who wants to connect to our 
+api application.
 
-# Create your tests here.
+'''
+
+
+import requests
+
+#URL = 'http://127.0.0.1:8000/all/'
+URL = 'http://127.0.0.1:8000/all/6'
+
+values = requests.get(URL)
+
+data = values.json()
+
+print(values)
+print(data)
