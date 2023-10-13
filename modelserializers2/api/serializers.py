9e5f4f485df379e4f0dc68233302697093ac0354
kapil-malviya <122611483+kapil-malviya@users.noreply.github.com>
2023-10-13 10:10:08 +0530

    changing or modifying model field attributes in serializers.py file

--- modelserializers2/api/serializers.py
+++ modelserializers2/api/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from .models import Student
-
+ 
 class StudentSerializer(serializers.ModelSerializer):
 	# name = serializers.CharField(read_only=True)                # for single field
 	class Meta:
